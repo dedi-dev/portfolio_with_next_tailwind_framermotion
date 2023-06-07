54f49f20dd97040c23a9ffd002bc0548e7c8dd78
dedi <id.dedi81@gmail.com>
2023-06-07 08:05:45 +0700

    init project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,25 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Montserrat } from "next/font/google";
+import Head from "next/head";
+import Navbar from "@/components/Navbar";
+
+const mont = Montserrat({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-mont",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main lang="en" className={`${mont.variable}`}>
+        <Navbar />
+        <Component {...pageProps} />;
+      </main>
+    </>
+  );
 }
---
+++ step_by_step.md
@@ -0,0 +1,5 @@
+1. init project NextJS
+2. setup Tailwind for NextJS (https://tailwindcss.com/docs/guides/nextjs)
+3. setup Font family (https://nextjs.org/docs/app/building-your-application/optimizing/fonts)
+4. create component navbar
+5. create component Logo
